0c0a33d946d1d0bbea65bd372c2d63c7eb3892a5
Lucas Gomes Santiago <lucasgomessantiago@gmail.com>
2021-01-31 21:53:30 -0300

    ajuste configuação pageable por causa do erro no swagger

--- src/main/java/br/com/gok/starwarsapi/resource/PlanetResource.java
+++ src/main/java/br/com/gok/starwarsapi/resource/PlanetResource.java
@@ -1,17 +1,13 @@
 package br.com.gok.starwarsapi.resource;
 
-import br.com.gok.starwarsapi.domain.postgres.Planet;
 import br.com.gok.starwarsapi.dto.PlanetDTO;
 import br.com.gok.starwarsapi.service.IPlanetService;
 import br.com.gok.starwarsapi.util.PageResponse;
 import lombok.RequiredArgsConstructor;
+import org.springdoc.api.annotations.ParameterObject;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
-import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @RequestMapping("/api/starwars/v1/planets")
 @RestController
@@ -22,10 +18,9 @@ public class PlanetResource {
 
 	@GetMapping
 	@ResponseStatus(HttpStatus.OK)
-	public PageResponse<PlanetDTO> get(@PageableDefault Pageable pageable){
+	public PageResponse<PlanetDTO> get(@ParameterObject Pageable pageable){
 		return service.listAll(pageable);
 	}
-
 
 	@DeleteMapping("/{id}")
 	@ResponseStatus(HttpStatus.OK)
